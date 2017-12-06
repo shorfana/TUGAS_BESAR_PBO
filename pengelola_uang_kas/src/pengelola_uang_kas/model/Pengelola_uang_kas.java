87137f878651c317cd675daeb22eccdb270d4cd1
iqbal <iqbalshorfana@gmail.com>
2017-12-06 15:01:56 +0700

    INI MASIH AWALNYA

---
+++ pengelola_uang_kas/src/pengelola_uang_kas/model/Pengelola_uang_kas.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pengelola_uang_kas.model;
+
+import pengelola_uang_kas.view.formlogin;
+
+/**
+ *
+ * @author shorfana
+ */
+public class Pengelola_uang_kas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        formlogin login = new formlogin();
+        login.setVisible(true);
+        
+ 
+    }
+    
+}
---
+++ pengelola_uang_kas/src/pengelola_uang_kas/model/input_dataclass.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pengelola_uang_kas.model;
+
+/**
+ *
+ * @author shorfana
+ */
+public class input_dataclass {
+    private String nama;
+    private int noKmr;
+    private int noLnt;
+    private double pNom;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getNoKmr() {
+        return  noKmr;
+    }
+
+    public void setNoKmr(int noKmr) {
+        this.noKmr = noKmr;
+    }
+
+    public int getNoLnt() {
+        return noLnt;
+    }
+
+    public void setNoLnt(int noLnt) {
+        this.noLnt = noLnt;
+    }
+
+    public double getpNom() {
+        return pNom;
+    }
+
+    public void setpNom(double pNom) {
+        this.pNom = pNom;
+    }
+    
+    
+    
+    
+}
